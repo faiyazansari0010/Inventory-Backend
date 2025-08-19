17e1053d07180e66b5a40e984f6208834115db57
Faiyaz <faiyazansari0010@gmail.com>
2025-08-19 21:51:54 +0530

    Testing

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const connectDB = require("./database/connection");
+
+connectDB();
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://inventory-frontend-app.netlify.app/",
+    ],
+    credentials: true,
+  })
+);
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+app.use(cookieParser());
+
+
+app.get("/", (req, res) => {
+  res.status(200).send("Canova Backend is Running");
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
